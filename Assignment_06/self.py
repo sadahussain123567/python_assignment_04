9ff17c2156ed9ce806f510c665c66c5fd8460984
sadahussain123567 <systemhang377@gmail.com>
2025-05-18 09:02:14 +0500

    update

--- Assignment_06/self.py
+++ Assignment_06/self.py
@@ -9,3 +9,24 @@ class Student:
     
 studentt=Student("Sadam Hussain","840")
 studentt.display()  
+
+# 2. Using cls
+class count:
+  counter =0
+  def __init__(self):
+    count.counter +=1
+  @classmethod  
+  def clss(cls):
+    print(f"Count is {cls.counter}")
+    
+neww=count()
+neww=count()
+neww=count()
+count.clss()
+
+# 3. Public Variables and Methods
+# Assignment:
+# Create a class Car with a public variable brand and a public method start(). Instantiate the class and access both from outside the class.
+class Car:
+  Brand="J."
+  
